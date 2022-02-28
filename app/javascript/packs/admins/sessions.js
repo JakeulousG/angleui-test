e4df3051688d77f0beba9ce55864a76a547ae616
JakeGiangan <jake@kodakollectiv.com>
2022-02-28 10:36:07 +0800

    fixed directory issue in import for sessions.js

--- app/javascript/packs/admins/sessions.js
+++ app/javascript/packs/admins/sessions.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 
 import $ from 'jquery';
-import initParsleyForPages from './angle/modules/pages/pages';
+import initParsleyForPages from '../angle/modules/pages/pages';
 
 $(function() {
     initParsleyForPages();
