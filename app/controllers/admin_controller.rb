058c162707de085faeacd6bb0cfc23c389c84a0b
JakeGiangan <jake@kodakollectiv.com>
2022-02-28 15:53:53 +0800

    modified/reworked the edit admins page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,4 +10,27 @@ class AdminController < ApplicationController
         @admins = Admin.all
         @admin = Admin.new
     end
+    
+    def edit
+        @admin = Admin.find(params[:id])
+    end
+
+    def update
+        @admin = Admin.find(params[:id])
+        params = current_admin.role == "Super Admin"? super_admin_params : admin_params
+        if @admin.update(params)
+        flash[:notice] = "Update successfully"
+        redirect_to admin_index_path
+        else
+        render 'edit'
+        end
+    end
+
+    protected
+    def admin_params
+        params.require(:admin).permit(:name, :email, :bio)
+    end
+    def super_admin_params
+        params.require(:admin).permit(:name, :email, :bio, :role)
+    end
 end
