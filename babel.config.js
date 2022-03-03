0a0b8ef376b4731478fe9658e1a90d802f35578d
JakeGiangan <jake@kodakollectiv.com>
2022-03-03 16:54:55 +0800

    segregated @babel/plugin

--- babel.config.js
+++ babel.config.js
@@ -43,8 +43,18 @@ module.exports = function(api) {
       '@babel/plugin-transform-destructuring',
       [
         '@babel/plugin-proposal-class-properties',
+        {
+          loose: true
+        }
+      ],
+      [
         "@babel/plugin-proposal-private-methods",
-        "@babel/plugin-proposal-private-property-in-object ",
+        {
+          loose: true
+        }
+      ],
+      [
+        "@babel/plugin-proposal-private-property-in-object",
         {
           loose: true
         }
