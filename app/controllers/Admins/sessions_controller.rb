ccf2c8189c98358efc609fa9079dde15230b710a
JakeGiangan <jake@kodakollectiv.com>
2022-03-04 11:17:26 +0800

    login works, but doesnt refresh

--- app/controllers/Admins/sessions_controller.rb
+++ app/controllers/Admins/sessions_controller.rb
@@ -2,18 +2,42 @@
 
 class Admins::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  # skip_before_action :verify_authenticity_token, only: :create
-  # respond_to :html, :js
+  skip_before_action :verify_authenticity_token, only: [:create, :destroy]
+  respond_to :html, :js
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    session['user_path'] = params[:admin]
+    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}")
+    sign_in(resource_name, resource)
+    message = I18n.t('devise.sessions.signed_in')
+    if request.xhr?
+      render :json => {:success => true, :login => true, :data => {:message => message}}
+    else
+      respond_to do |format|
+        format.html {redirect_to admins_sign_in_path}
+      end
+    end
+  end
 
+  def failure
+    user = User.find_by_email(session['user_auth'][:email])
+     message = I18n.t('devise.failure.invalid')
+      if user =! nil || user.active_for_authentication?
+        flash[:error]= message unless request.xhr?
+     else
+       flash[:error]= user.inactive_message unless request.xhr?
+       message = user.inactive_message    
+      end
+    respond_to do |format|
+      format.json {render :json => {:authentication => 'failure', :data => {:message => message, :cause => 'invalid'} } }
+      format.html {redirect_to '/users/sign_in'}
+    end
+  end
   # DELETE /resource/sign_out
   # def destroy
   #   super
